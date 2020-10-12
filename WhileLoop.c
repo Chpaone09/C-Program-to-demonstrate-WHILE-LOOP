39833c42459ceecd6ad4b4184bc183b6806c041f
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:17:52 +0530

    Program to demonstrate WHILE LOOP in c

---
+++ WhileLoop.c
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate WHILE LOOP in c.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int i=1;
+    printf("\n\n\t Demonstration of WHILE LOOP in C >> ");
+    while(i <= 5){
+        printf(" %d", i++);
+    }
+}
